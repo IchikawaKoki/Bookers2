251fa8638df3e05d973dbac5830bc1b08783c63d
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2020-12-05 14:36:56 +0000

    [Add]index,show,edit

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+   resources :books, only:[:create,:index,:show,:edit,:update,:destroy]
 end
